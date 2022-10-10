1da6371add8e757b7b7d12e70b405833d7018763
dmilanop <dmilanop28@gmail.com>
2022-10-10 13:32:48 -0400

    Logrado el objetivo :)

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,23 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
+
+	const[count, setCount] = useState(0)
+
+	useEffect(() => {
+		const increase = setInterval(() => {
+			setCount(count + 1)
+		}, 1000)
+
+		return () => {
+			clearInterval(increase)
+		}
+	}, [count]);
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>Counter: {count}</h1>
 		</div>
 	);
 };
